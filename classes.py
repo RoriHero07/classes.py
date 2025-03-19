46ff6b41022e677ca2bf8c52967582f1f69966f5
RoriHero07 <raduquedeestrada@icloud.com>
2025-03-18 19:37:52 -0500

    Initial Check In

--- classes.py
+++ classes.py
@@ -12,10 +12,34 @@ class Student:
         for grade in self.grades:
             total_score = total_score+grade
         final_grade = total_score/len(self.grades)
+        return final_grade
+
+class Classroom:
+    def __init__(self):
+        self.students = []
+
+    def add(self, student: Student):
+        self.students.append(student)
+
+    def calculate(self):
+        total_score = 0
+        for student in self.students:
+            total_score = total_score + student.calculate()
+        final_grade = total_score/len(self.students)
         print(final_grade)
 
+
+classroom = Classroom()
 scores = [20, 40, 60, 80]
-my_student = Student("Danny")
+danny = Student("Danny")
 for score in scores:
-    my_student.add(score)
-my_student.calculate()
+    danny.add(score)
+classroom.add(danny)
+
+scores = [50, 70, 30, 40]
+christina = Student("Christina")
+for score in scores:
+    christina.add(score)
+classroom.add(christina)
+
+classroom.calculate()
